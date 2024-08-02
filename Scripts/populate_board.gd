76a36dc4d4e92ec870fdd428c2407fb892ef6263
Lunar Vagabond (LV) <lvagabond@dcorps.dev>
2024-08-02 10:22:42 -0400

    [noissue] - Enemies now spawn in and do damage AFTER left for a round

--- Scripts/populate_board.gd
+++ Scripts/populate_board.gd
@@ -27,13 +27,20 @@ func _unhandled_input(event):
     if Globals.is_palyers_turn and event.is_action_pressed(GameConstants.IA_SUBMIT_TURN):
         Globals.is_palyers_turn = false
         var selected_tiles = _find_selected_tiles()
-        _replace_tiles(selected_tiles)
+        _replace_tiles_with_empty(selected_tiles)
         _handle_enemy_turn()
+        var empty_tiles = _find_empty_tiles()
+        if len(empty_tiles) > 0:
+            _replace_tiles(empty_tiles, false)
         Globals.is_palyers_turn = true
         print("Do this later")
 
 func _handle_enemy_turn():
-    pass
+    var dmg = 0
+    for t: GameTile in game_board.get_children():
+        if t.tile_type == Tile.TileType.Enemy:
+            dmg += 1
+    print("Damage Done: ", dmg)
 
 func _find_selected_tiles() -> Array[GameTile]:
     var selected_tiles: Array[GameTile] = []
@@ -42,18 +49,33 @@ func _find_selected_tiles() -> Array[GameTile]:
             selected_tiles.append(txtbtn)
     return selected_tiles
 
-func _replace_tiles(selected_tiles: Array[GameTile]):
+func _find_empty_tiles() -> Array[GameTile]:
+    var empty: Array[GameTile] = []
+    for t: GameTile in game_board.get_children():
+        if t.tile_type == Tile.TileType.Empty:
+            empty.append(t)
+    return empty
+
+func _replace_tiles(selected_tiles: Array[GameTile], allow_empty: bool):
     for t: GameTile in selected_tiles:
-        var new_tile = _new_tile(t.index_on_board)
+        var new_tile = _new_tile(t.index_on_board, allow_empty)
         game_board.remove_child(t)
         game_board.add_child(new_tile)
         game_board.move_child(new_tile, new_tile.index_on_board)
         t.queue_free()
-    
+
+func _replace_tiles_with_empty(selected_tiles: Array[GameTile]):
+    for t: GameTile in selected_tiles:
+        var new_tile = _new_specific_tile(t.index_on_board, Tile.TileType.Empty)
+        game_board.remove_child(t)
+        game_board.add_child(new_tile)
+        game_board.move_child(new_tile, new_tile.index_on_board)
+        t.queue_free()
+
 func _load_tile_group():
     var tiles_array: Array[Tile] = []
     tile_resource_group.load_all_into(tiles_array)
-    for t in tiles_array:
+    for t: Tile in tiles_array:
         tiles[Tile.TileType.keys()[t.tile_type]] = t
 
 func _set_dashboard():
@@ -63,11 +85,11 @@ func _set_dashboard():
 
 func populate_game_board():
     for i in range(56):
-        var tile = _new_tile(i)
+        var tile = _new_tile(i, false)
         game_board.add_child(tile)
 
-func _new_tile(idx: int) -> GameTile:
-    var tile_data: Tile = get_random_dict_key()
+func _new_tile(idx: int, can_be_empty: bool) -> GameTile:
+    var tile_data: Tile = get_random_dict_key(can_be_empty)
     var tile: GameTile = tile_scene.instantiate()
     tile.tile_type = tile_data.tile_type
     tile.name = str(idx)
@@ -76,11 +98,22 @@ func _new_tile(idx: int) -> GameTile:
     tile.connect("pressed", Callable(self, "_on_Button_pressed").bind(tile)) # Bind to the Callable for custom signal functionality
     return tile
 
-func get_random_dict_key() -> Tile:
+func _new_specific_tile(idx: int, target_type: Tile.TileType) -> GameTile:
+    var tile_data: Tile = tiles[Tile.TileType.keys()[target_type]]
+    print(tile_data)
+    var tile: GameTile = tile_scene.instantiate()
+    tile.tile_type = tile_data.tile_type
+    tile.name = str(idx)
+    tile.index_on_board = idx
+    tile.texture_normal = tile_data.icon
+    tile.connect("pressed", Callable(self, "_on_Button_pressed").bind(tile)) # Bind to the Callable for custom signal functionality
+    return tile
+
+func get_random_dict_key(empty_allowed: bool) -> Tile:
     var keys = tiles.keys()
     var random_index = randi() % keys.size()
     var tile: Tile = tiles[keys[random_index]]
-    while tile.tile_type == Tile.TileType.Empty:
+    while not empty_allowed and tile.tile_type == Tile.TileType.Empty:
         random_index = randi() % keys.size()
         tile = tiles[keys[random_index]]
     return tile
@@ -90,6 +123,8 @@ func index_to_vector2(index: int) -> Vector2:
     var row = index / board_width
     var column = index % board_width
     return Vector2(column, row)
+
+# ------- Signals ----- #
 
 func _on_Button_pressed(button: BaseButton): # Normally "pressed" does not take in a button but Bind to callable allows for this (?)
     # print("Tile Clicked: ", button.name)
